d5523a26c1eacec7103b3b2ebaf2fed63d94c34f
Desi <facebookaa@abv.bg>
2014-07-21 22:11:21 +0300

    rps sinatra

--- rps_game_sinatra.rb
+++ rps_game_sinatra.rb
@@ -1,22 +1,27 @@
 require 'sinatra'
+require 'erb'
+
+get '/' do
+  erb :index
+end
 
 get '/play/:move' do
-  user_move = params[:move]
+  @user_move = params[:move]
   rules = {'rock' => 'scissors', 'scissors' => 'paper', 'paper' => 'rock'}
   moves = rules.keys
 
-  if not moves.include? user_move
-    next "Wrong move: #{user_move}"
+  if not moves.include? @user_move
+    @result = "Wrong move: #{@user_move}"
   end
 
-  comp_move = moves.sample
+  @comp_move = moves.sample
 
-  if user_move == comp_move
-    next "Computer choose: #{comp_move}. It's a tie."
-  elsif rules[user_move] == comp_move
-    next "Computer choose: #{comp_move}. You win!"
+  if @user_move == @comp_move
+    @result = "It's a tie. Try again!"
+  elsif rules[@user_move] == @comp_move
+    @result = "You are winner!"
   else
-    next "Computer choose: #{comp_move}. Sorry, you lose!"
+    @result = "Sorry, you lose!"
   end
+  erb :play
 end
-
